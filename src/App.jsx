36f064b4fdfd4a1f567d78362987782e81a2e6d3
LucaStellato <lucastellato435@gmail.com>
2026-02-20 11:07:27 +0100

    milestone 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 function App() {
+  const [data, setData] = useState([])
+  const [query, setQuery] = useState('')
+  useEffect(() => {
+    if (query.trim() == '') {
+      setData([])
+      return
+    }
+    axios.get(`http://localhost:3333/products?search=${query}`)
+      .then((response) => {
+        setData(response.data)
+
+      })
+  }, [query])
+  console.log(data)
 
 
   return (
     <>
+      <input
+        type='text'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)} />
+
+
+
+
+      {data.length > 0 && (
+        <div>
+          {data.map((item) => (
+            <div key={item.id}>
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
+
+
 
 
     </>
